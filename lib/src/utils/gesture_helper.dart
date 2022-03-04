c46b92ddaca9626a92037cf0e4a81e8d4df84e0d
William Suyanto <william.suyanto@gmail.com>
2022-03-05 01:37:39 +0700

    add gesture helper

---
+++ lib/src/utils/gesture_helper.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+import 'dart:ui';
+
+double coordinatesToRadians(Offset center, Offset coords) {
+  final double a = coords.dx - center.dx;
+  final double b = center.dy - coords.dy;
+  return atan2(b, a);
+}
+
+double radiansToPercentage(double radians) {
+  final double normalized = radians < 0 ? -radians : 2 * pi - radians;
+  final double percentage = (100 * normalized) / (2 * pi);
+  return (percentage + 25) % 100;
+}
+
+int percentageToValue(double percentage, int intervals) => ((percentage * intervals) / 100).round();
