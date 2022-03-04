ba33d9c8efc6ef15217647aa6537809db4cfabb7
William Suyanto <william.suyanto@gmail.com>
2022-03-04 15:18:37 +0700

    add clock face background

--- lib/src/widgets/clock_widget.dart
+++ lib/src/widgets/clock_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_app/src/constants/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ClockWidget extends StatelessWidget {
   const ClockWidget({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class ClockWidget extends StatelessWidget {
       child: Stack(
         children: <Widget>[
           _buildBodyClock(),
+          _buildFaceClock(),
         ],
       ),
     );
@@ -37,6 +39,55 @@ class ClockWidget extends StatelessWidget {
             blurRadius: 5.0,
           )
         ],
+      ),
+    );
+  }
+
+  Widget _buildFaceClock() {
+    return Padding(
+      padding: EdgeInsets.all(40.0.w),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: RadialGradient(
+              colors: <Color>[
+                AppColors.kWhite2,
+                AppColors.kGrey,
+              ],
+              stops: <double>[
+                0.9,
+                1.0,
+              ],
+            ),
+            color: AppColors.kWhite2,
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: AppColors.kGrey2,
+                blurRadius: 50.0,
+              ),
+            ],
+          ),
+          child: Stack(
+            children: <Widget>[
+              //TODO: clock numbers
+              //TODO: clock hands
+              // Center Dot
+              Center(
+                child: Container(
+                  width: 5.0.w,
+                  height: 5.0.w,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
