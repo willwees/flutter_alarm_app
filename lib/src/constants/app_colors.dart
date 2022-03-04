290cb41a3fc3691cb47b4f550cc33e4580af2035
William Suyanto <william.suyanto@gmail.com>
2022-03-04 13:47:34 +0700

    add AppColors

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color kBlack = Color(0xFF000000);
+  static const Color kBlack2 = Color(0xFF1A1A1A);
+  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kWhite2 = Color(0xFFF3F3F3);
+  static const Color kWhite3 = Color(0xFFFAFAFA);
+  static const Color kWhite4 = Color(0xFFFDF9FF);
+  static const Color kGrey = Color(0xFFA7A7A7);
+  static const Color kGrey2 = Color(0xFF787878);
+  static const Color kRed = Color(0xFFAD595C);
+}
