4094eeae1210e02d64d9188f01c045128fcc11f5
William Suyanto <william.suyanto@gmail.com>
2022-03-04 13:47:01 +0700

    add screen util

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_app/src/screens/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Alarm App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: const Size(750, 1334),
+      builder: () => MaterialApp(
+        title: 'Flutter Alarm App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
