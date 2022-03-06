5dbecd76260e4a907d95b7af940036dd81d4f35c
William Suyanto <william.suyanto@gmail.com>
2022-03-06 10:10:55 +0700

    add BarChartWidget to DetailScreen

--- lib/src/screens/detail_screen.dart
+++ lib/src/screens/detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_app/src/utils/date_helper.dart';
+import 'package:flutter_alarm_app/src/widgets/detail/bar_chart_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DetailScreen extends StatefulWidget {
   final String? arguments;
@@ -11,28 +13,39 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  late final String? _dateTimeString;
+  late final DateTime _alarmDateTime;
+  late final int _alarmDiffSeconds;
 
   @override
   void initState() {
     super.initState();
-    _dateTimeString = widget.arguments;
-    debugPrint('dateTimeString: $_dateTimeString');
+    _alarmDateTime = widget.arguments != null ? DateTime.parse(widget.arguments!) : DateTime(1900);
+    _alarmDiffSeconds = DateTime.now().difference(_alarmDateTime).inSeconds;
+    debugPrint('_alarmDateTime: $_alarmDateTime');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Alarm App'),
+        title: const Text('Alarm Details'),
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            if (_dateTimeString != null)
-              Text('Alarm At: ${DateHelper.dateTimeToString(DateTime.parse(_dateTimeString!))}'),
-            Text('Now: ${DateHelper.dateTimeToString(DateTime.now())}'),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(8.0.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Alarm At: ${DateHelper.dateTimeToString(_alarmDateTime)}'),
+              Text('Now: ${DateHelper.dateTimeToString(DateTime.now())}'),
+              Text('Diff seconds: $_alarmDiffSeconds'),
+              SizedBox(height: 100.0.w),
+              BarChartWidget(
+                alarmDateTime: _alarmDateTime,
+                alarmDiffSeconds: _alarmDiffSeconds,
+              ),
+            ],
+          ),
         ),
       ),
     );
