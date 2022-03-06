bedbbf64333d414acf20f3c4c797db50f98b977c
William Suyanto <william.suyanto@gmail.com>
2022-03-06 10:10:33 +0700

    add BarChartWidget

---
+++ lib/src/widgets/detail/bar_chart_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alarm_app/src/utils/date_helper.dart';
+import 'package:flutter_alarm_app/src/utils/painter/bar_axis_painter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BarChartWidget extends StatelessWidget {
+  final DateTime alarmDateTime;
+  final int alarmDiffSeconds;
+
+  const BarChartWidget({
+    Key? key,
+    required this.alarmDateTime,
+    required this.alarmDiffSeconds,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        //TODO: temp color
+        color: Colors.green,
+        padding: EdgeInsets.all(16.0.w),
+        child: CustomPaint(
+          painter: BarAxisPainter(
+            alarmText: DateHelper.dateTimeToString(alarmDateTime),
+            multiplier: alarmDiffSeconds ~/ 5,
+          ),
+        ),
+      ),
+    );
+  }
+}
